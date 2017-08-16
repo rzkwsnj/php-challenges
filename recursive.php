f48b6949c8d0428742b9f48e2c27162b796a7db7
Rizki Wisnuaji <5307162+rizkiwisnuaji@users.noreply.github.com>
2017-08-16 20:29:48 +0700

    Create recursive.php

---
+++ recursive.php
@@ -0,0 +1,28 @@
+<?php
+
+$input = -~0 + 3;
+
+function factorial($n) {
+	
+	if ($n <= 0) {
+		
+		return "Must be greater than 0 !";
+		
+	} else {
+	
+		if ($n === 1) {
+		
+			return 1;
+			
+		} else {
+		
+			return factorial($n-1) * $n;
+			
+		}
+		
+	}
+	
+		
+}
+
+print factorial($input);
