e422bbd797554e196eda6b5363f8390cda47fd53
Rizki Wisnuaji <5307162+rizkiwisnuaji@users.noreply.github.com>
2017-08-17 21:04:15 +0700

    Update remove-exclamation-mark-from-the-end-of-string.php

--- remove-exclamation-mark-from-the-end-of-string.php
+++ remove-exclamation-mark-from-the-end-of-string.php
@@ -6,5 +6,5 @@ print substr($s,-1) === "!" ? substr( $s, 0, -1) : $s;
 
 // print preg_replace('/!$/', "", $s);
 
-// $result = $s{strlen($s) -1 };
-// print $result == "!" ? substr($s, 0, -1) : $s;
+// $result = $s{ strlen( $s ) -1 };
+// print $result == "!" ? substr( $s, 0, -1) : $s;
